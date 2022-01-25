d3a43d0feebbb7c1ae1d3c3c83c9d279bbf09263
kadennguyen0329 <kadennguyen0329@gmail.com>
2022-01-25 17:26:38 -0500

    Create Index.java

---
+++ src/main/java/frc/robot/subsystems/Index.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class Index extends SubsystemBase{
+    private CANSparkMax motor1;
+    private CANSparkMax motor2;
+
+    public FlyWheel()
+    {
+        motor1 = new CANSparkMax(0, MotorType.kBrushless);
+        motor2 = new CANSparkMax(0, MotorType.kBrushless);
+    }
+
+    public void start()
+    {
+        motor1.set(0.25);
+        motor2.set(0.25);
+     }
+    
+      public void stop()
+      {
+        motor1.disable();
+        motor2.disable();
+      }
+}
