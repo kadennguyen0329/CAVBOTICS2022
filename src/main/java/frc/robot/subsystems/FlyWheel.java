649bb368d43cda3012a2a6f90d1705faecdb0e97
kadennguyen0329 <kadennguyen0329@gmail.com>
2022-01-25 16:54:38 -0500

    asd

---
+++ src/main/java/frc/robot/subsystems/FlyWheel.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+
+
+public class FlyWheel extends SubsystemBase{
+    private CANSparkMax rightMotor;
+    private CANSparkMax leftMotor1;
+
+    public FlyWheel()
+    {
+
+    }
+
+}
