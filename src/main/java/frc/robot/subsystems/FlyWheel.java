8c21b12663284ac91a7c4ba8c488588644058547
kadennguyen0329 <kadennguyen0329@gmail.com>
2022-01-25 17:02:48 -0500

    flywheel subsystem

--- src/main/java/frc/robot/subsystems/FlyWheel.java
+++ src/main/java/frc/robot/subsystems/FlyWheel.java
@@ -2,15 +2,27 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
-
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class FlyWheel extends SubsystemBase{
     private CANSparkMax rightMotor;
-    private CANSparkMax leftMotor1;
+    private CANSparkMax leftMotor;
 
     public FlyWheel()
     {
-
+        rightMotor = new CANSparkMax(0, MotorType.kBrushless);
+        leftMotor = new CANSparkMax(0, MotorType.kBrushless);
     }
 
+    public void start(double shooterSpeed)
+    {
+        rightMotor.set(0.25);
+        leftMotor.set(0.25);
+     }
+    
+      public void stop()
+      {
+        rightMotor.disable();
+        leftMotor.disable();
+      }
 }
