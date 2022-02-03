105617fa77b686bf4228a7f501f9a6585f9fcd49
Tony <totran0331@gmail.com>
2022-02-03 17:39:43 -0500

    Added SwerveDrive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.SwerveDriveTrain;
+import edu.wpi.first.wpilibj.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -18,6 +20,8 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  public SwerveDriveTrain swerveDrive;
+  private XboxController controller;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -25,9 +29,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+    swerveDrive = new SwerveDriveTrain(1.5, 3, 4, 1, 2, 5, 6, 7, 8);
+    controller = new XboxController(0);
   }
 
   /**
@@ -81,7 +84,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    swerveDrive.updatePeriodic(controller.getX(Hand.kLeft), controller.getY(Hand.kLeft), controller.getX(Hand.kRight));
+  }
 
   @Override
   public void testInit() {
