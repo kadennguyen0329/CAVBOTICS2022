ce1b23f50ce8a85713c835face143d34f4a62f7d
Isabelle Vu <vu.isa04@gmail.com>
2022-02-10 16:01:34 -0500

    Key bindings stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,10 +5,25 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.subsystems.Flywheel;
+import frc.robot.subsystems.Hood;
+import frc.robot.subsystems.Index;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.PhotonVision;
+import frc.robot.subsystems.PickUpWheel;
+import frc.robot.commands.IndexCommand;
+import frc.robot.commands.IntakeCommand;
+import frc.robot.commands.PhotonVisionCommand;
+import frc.robot.commands.PickUpWheelCommand;
+import frc.robot.commands.TurnOnFlywheel;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -17,6 +32,14 @@ import edu.wpi.first.wpilibj2.command.Command;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+  public final static Flywheel flywheel = new Flywheel();
+  public final static Hood hood = new Hood();
+  public final static Index index = new Index();
+  public final static Intake intake = new Intake();
+  public final static Limelight limelight = new Limelight();
+  public final static PhotonVision photonvision = new PhotonVision();
+  public final static PickUpWheel pickUpWheel = new PickUpWheel();
+
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
@@ -34,7 +57,11 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    new JoystickButton(Constants.controller, 4).whenPressed(new PickUpWheelCommand(pickUpWheel));
+    new JoystickButton(Constants.controller, 3).whenPressed(new IntakeCommand(intake));
+    new JoystickButton(Constants.controller, 1).whenPressed(new IndexCommand(index));
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
