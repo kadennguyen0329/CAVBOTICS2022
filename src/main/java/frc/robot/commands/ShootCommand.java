b819df20f679eabe576a53f45ee6be632822dbff
Aaron Amartuvshin <45109268+aarontuvshin@users.noreply.github.com>
2022-02-25 10:50:02 -0500

    updated intake and shooter system

---
+++ src/main/java/frc/robot/commands/ShootCommand.java
@@ -0,0 +1,58 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Index;
+import frc.robot.subsystems.Flywheel;
+import frc.robot.subsystems.Sensor;
+
+
+public class ShootCommand extends CommandBase {
+    private Flywheel flywheel;
+    private Index index; 
+    private Sensor sensor;
+
+    public ShootCommand(Flywheel f, Index i, Sensor s, int desiredRPM){
+        flywheel = f;
+        index = i;
+        sensor = s;
+        addRequirements(flywheel, index, sensor);
+    }
+
+    @Override
+    public void initialize(){
+        flywheel.start();
+        
+
+    }
+
+    @Override
+    public void execute(){
+        if(flywheel.fullSpeed() && sensor.innerIsClosed()){
+            index.spinInnerIndex();
+        }
+        if(!sensor.innerIsClosed() && sensor.outerIsClosed()){
+            index.spinInnerIndex();
+
+        }
+
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        index.stopInnerIndex();
+        index.stopOuterIndex();
+        flywheel.stop();
+
+    }
+
+    @Override
+    public boolean isFinished(){
+        if(!sensor.innerIsClosed() && !sensor.outerIsClosed()){
+            return true;
+        }
+        return false;
+
+    }
+
+
+}
--- src/main/java/frc/robot/subsystems/Flywheel.java
+++ src/main/java/frc/robot/subsystems/Flywheel.java
@@ -1,28 +1,43 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Flywheel extends SubsystemBase{
     private CANSparkMax rightMotor;
     private CANSparkMax leftMotor;
+    private RelativeEncoder encoder;
+    private int desiredRPM; 
 
-    public Flywheel()
+    public Flywheel(int v)
     {
-        rightMotor = new CANSparkMax(0, MotorType.kBrushless);
-        leftMotor = new CANSparkMax(0, MotorType.kBrushless);
+        rightMotor = new CANSparkMax(Constants.flyWheelPort, MotorType.kBrushless);
+        leftMotor = new CANSparkMax(Constants.flyWheelPort2, MotorType.kBrushless);
+        desiredRPM = v;
     }
 
     public void start()
     {
-        rightMotor.set(0.25);
-        leftMotor.set(0.25);
+        rightMotor.set(0.8);
+        leftMotor.set(0.8);
      }
     
       public void stop()
       {
         rightMotor.disable();
         leftMotor.disable();
+      }
+
+      public boolean fullSpeed(){
+        encoder = rightMotor.getEncoder();
+
+        if(encoder.getVelocity() > desiredRPM){
+          return true;
+        }
+        return false;
       }
 }
