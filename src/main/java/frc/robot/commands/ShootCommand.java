a7f5e5d15925d89a39a0cef1893a4dd8a40b52c3
Tony <totran0331@gmail.com>
2022-03-05 15:28:50 -0500

    Updated shooter

--- src/main/java/frc/robot/commands/ShootCommand.java
+++ src/main/java/frc/robot/commands/ShootCommand.java
@@ -1,55 +1,65 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.subsystems.Index;
 import frc.robot.subsystems.Flywheel;
 import frc.robot.subsystems.Sensor;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.PickupWheel;
 
 public class ShootCommand extends CommandBase {
     private Flywheel flywheel;
-    private Index index; 
+    private Index index;
     private Sensor sensor;
+    private PickupWheel pickup;
+    private Shooter shooter;
 
-    public ShootCommand(Flywheel f, Index i, Sensor s, int desiredRPM){
+    public ShootCommand(Flywheel f, Index i, Sensor s, PickupWheel p, Shooter sh, int desiredRPM) {
         flywheel = f;
         index = i;
         sensor = s;
-        addRequirements(flywheel, index, sensor);
-    }
-
-    @Override
-    public void initialize(){
-        flywheel.start();
+        pickup = p;
+        shooter = sh;
+        addRequirements(flywheel, index, sensor, pickup, shooter);
         
     }
 
     @Override
-    public void execute(){
-        if(flywheel.fullSpeed() && sensor.innerIsClosed()){
-            index.spinInnerIndex();
-        }
-        if(!sensor.innerIsClosed() && sensor.outerIsClosed()){
-            index.spinInnerIndex();
-
-        }
-
+    public void initialize() {
+        flywheel.start();
     }
 
     @Override
-    public void end(boolean interrupted){
+    public void execute() {
+        if (flywheel.fullSpeed()) {
+            // replace value, temp for now
+            // to replace with aim
+            shooter.setWheel(9);
+
+            index.spinInnerIndex();
+            index.spinOuterIndex();
+
+            pickup.start();
+
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
         index.stopInnerIndex();
+        index.stopOuterIndex();
         flywheel.stop();
-
+        pickup.stop();
+        shooter.setWheel(0);
     }
 
     @Override
-    public boolean isFinished(){
-        if(!sensor.innerIsClosed() && !sensor.outerIsClosed()){
+    public boolean isFinished() {
+        if (Constants.controller.getBButton()) {
             return true;
         }
         return false;
-
     }
-
 
 }
---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.SparkMaxPIDController;
+import frc.robot.Constants;
+
+public class Shooter extends SubsystemBase {
+    private CANSparkMax shooterLeft;
+    private CANSparkMax shooterRight;
+    private SparkMaxPIDController pidLeft;
+    private SparkMaxPIDController pidRight;
+
+    public Shooter() {
+        shooterLeft = new CANSparkMax(Constants.shooterIdOne, MotorType.kBrushless);
+        shooterRight = new CANSparkMax(Constants.shooterIdTwo, MotorType.kBrushless);
+        pidLeft = shooterLeft.getPIDController();
+        pidRight = shooterRight.getPIDController();
+        pidLeft.setOutputRange(-1, 1);
+        pidRight.setOutputRange(-1, 1);
+        pidLeft.setP(0.1);
+        pidRight.setP(0.1);
+    }
+
+    public void setWheel(double RPM) {
+        shooterLeft.set(RPM);
+        shooterRight.set(-RPM);
+    }
+}
