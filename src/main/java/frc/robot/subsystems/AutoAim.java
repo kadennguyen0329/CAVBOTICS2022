577304ddcac55fee9d649e88f5425cc0ad260866
Tony <totran0331@gmail.com>
2022-03-05 15:29:15 -0500

    Updated autoaim

--- src/main/java/frc/robot/subsystems/AutoAim.java
+++ src/main/java/frc/robot/subsystems/AutoAim.java
@@ -1,62 +1,59 @@
 package frc.robot.subsystems;
- 
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
- 
+
 public class AutoAim {
-    private final NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight-sam");
-    private final NetworkTableEntry pipeline = table.getEntry("pipeline");
- 
-    public AutoAim(){
-       
+    private NetworkTable table;
+    private NetworkTableEntry pipeline;
+    private PIDController pid;
+
+    public AutoAim() {
+        table = NetworkTableInstance.getDefault().getTable("limelight-sam");
+        pipeline = table.getEntry("pipeline");
+        pid = new PIDController(0.004, 0, 0);
+        pid.setTolerance(3);
     }
- 
-    public double getXOffset(){
+
+    public double getXOffset() {
         return table.getEntry("tx").getDouble(0);
     }
- 
-    public double getYOffset(){
+
+    public double getYOffset() {
         return table.getEntry("ty").getDouble(0);
     }
- 
-    public int hasTarget(){
-        return (int)table.getEntry("tv").getDouble(0);
+
+    public int hasTarget() {
+        return (int) table.getEntry("tv").getDouble(0);
     }
-    public double[] aimPeriodic(){
-        double[] values = {0,0,0};
-        if (hasTarget() == 0){
-            System.out.println("Warning, no target in sight");
-            return values;
-        }
-        //first move the drivetrain so that x offset is 0
-        double distance = 8 / Math.tan(30);
-        values[2] = setDriveTrainAim(getXOffset());
-        // setHoodAim(getYOffset(), distance);
-        return values;
- 
+
+    public double getXDistance() {
+        // limelight is fixed at 30 degrees
+        double realAngle = 21 + getYOffset();
+        return 7.4 / Math.tan(Math.toRadians(realAngle));
     }
- 
-    public void setHoodAim(double yOffset, double distance){
-        // double absoluteAngle =
-        // double sendAngle = yOffset +
+
+    public double setHoodAim() {
+        double distance = getXDistance();
+        // this is some random correlation, 5 feet away equals hood at 10 degrees, 10
+        // feet away equals 20 degrees, etc
+        return Math.abs(distance * 2);
     }
- 
-    public double setDriveTrainAim(double xOffSet){
-        //Don't move the drivetrain if it's already within the tolerance
-        if (Math.abs(xOffSet) <= 3){
-            return 0;
-        }
- 
-        if (xOffSet < 0){
-        //if x offset is negative, then we need to turn the drive train to the right
-            return -0.3;
-        }
-        else{
-            return 0.3;
-        }
+
+    public double testDistance(){
+        double distance = getXDistance();
+        return distance;
+    }
+    public double getSpeed() {
+        // Don't move the drivetrain if it's already within the tolerance
+        return pid.calculate(this.getXOffset());
+    }
+
+    public double getRPM() {
+        double baseRPM = 1500;
+        double distance = getXDistance();
+        return baseRPM + (distance * 50);
     }
 }
- 
- 
-
