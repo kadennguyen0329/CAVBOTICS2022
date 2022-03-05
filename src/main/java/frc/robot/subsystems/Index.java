28d2fe5c3ec6e94d955a9aeafef1dbbf535b4df6
Tony <totran0331@gmail.com>
2022-03-05 15:29:02 -0500

    Updated index

--- src/main/java/frc/robot/subsystems/Index.java
+++ src/main/java/frc/robot/subsystems/Index.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import frc.robot.Constants;
 
