11fdbbb663a177f153846d309d50070de16637ff
Tony <totran0331@gmail.com>
2022-03-05 15:28:37 -0500

    Updated intake

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -1,42 +1,42 @@
 package frc.robot.commands;
+
 import frc.robot.subsystems.Intake;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 
-public class IntakeCommand extends CommandBase{
-    private Intake intake; 
-   // private Sensor ultrasonics;
-    
-    public IntakeCommand(Intake x) {
-        intake = x;
-        //ultrasonics = y;
-        // Use addRequirements() here to declare subsystem dependencies.
-        addRequirements(intake);
-      }
-    
-      // Called when the command is initially scheduled.
-      @Override
-      public void initialize() {
-        //intake.extend();
-      }
-    
-      // Called every time the scheduler runs while the command is scheduled.
-      @Override
-      public void execute() {
-          intake.spinIntake();
-      }
-    
-      // Called once the command ends or is interrupted.
-      @Override
-      public void end(boolean interrupted) {
-          intake.stopIntake();
-         // intake.retract();
-      }
-    
-      // Returns true when the command should end.
-      @Override
-      public boolean isFinished() {
-        // return (Constants.controller.getXButton() || );
-        return true;
-      }
+public class IntakeCommand extends CommandBase {
+  private Intake intake;
+
+  public IntakeCommand(Intake x) {
+    intake = x;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(intake);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    intake.extend();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (Constants.controller.getYButton()) {
+      intake.spinIntake();
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    intake.stopIntake();
+    intake.retract();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return Constants.controller.getXButton();
+  }
 }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,41 +1,42 @@
 package frc.robot.subsystems;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import frc.robot.Constants;
 
-public class Intake extends SubsystemBase{
-    private CANSparkMax intake;    
+public class Intake extends SubsystemBase {
+    private CANSparkMax intake;
     private Compressor compressor;
-    //private DoubleSolenoid mainSolenoid;
-    
+    private DoubleSolenoid mainSolenoid;
+
     public Intake() {
-    intake = new CANSparkMax(Constants.intakePort,MotorType.kBrushless);
-    //mainSolenoid = new DoubleSolenoid(10, PneumaticsModuleType.CTREPCM, Constants.mainSolenoid1, Constants.mainSolenoid2);
-    //compressor = new Compressor(PneumaticsModuleType.CTREPCM);
-    //compressor.enableDigital();
+        intake = new CANSparkMax(Constants.intakePort, MotorType.kBrushless);
+        mainSolenoid = new DoubleSolenoid(10, PneumaticsModuleType.CTREPCM, Constants.mainSolenoid1,
+                Constants.mainSolenoid2);
+        compressor = new Compressor(PneumaticsModuleType.CTREPCM);
+        compressor.enableDigital();
     }
 
-    public void spinIntake(){
+    public void spinIntake() {
         intake.set(.20);
     }
 
     public void extend() {
-        //mainSolenoid.toggle();
+        compressor.enableDigital();
+        mainSolenoid.toggle();
     }
 
-    public void retract()
-    {
-        //mainSolenoid.toggle();
+    public void retract() {
+        compressor.disable();
+        mainSolenoid.toggle();
     }
 
-    public void stopIntake(){
+    public void stopIntake() {
         intake.set(0);
     }
-    
+
 }
