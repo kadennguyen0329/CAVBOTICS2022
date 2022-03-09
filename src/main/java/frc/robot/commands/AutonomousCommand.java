ba55acabdfa75eea4023f7de384c940f3aaa428a
Aaron Amartuvshin <45109268+aarontuvshin@users.noreply.github.com>
2022-03-09 12:46:44 -0500

    cleaner autonomous

--- src/main/java/frc/robot/commands/AutonomousCommand.java
+++ src/main/java/frc/robot/commands/AutonomousCommand.java
@@ -17,16 +17,8 @@ public class AutonomousCommand extends CommandBase {
     private Shooter shooter;
     private SwerveDriveTrain swerveDrive;
     private Timer timer;
-    private boolean shot = false;
     private double power;
-    private boolean aim = false;
-    private boolean turned = false;
-    private boolean intakeOn = false;
-    private boolean moved = false;
-    private boolean turnedAgain = false;
-    private boolean retractedIntake = false;
-    private boolean aim2 = false;
-    private boolean shoot2 = false;
+    private int step;
 
     
 
@@ -41,6 +33,7 @@ public class AutonomousCommand extends CommandBase {
         swerveDrive = swerve;
         timer = new Timer();
         power = 0;
+        step = 0;
 
         addRequirements(autoaim, climber, hood, innerIndex, intake, outerIndex, shooter, swerveDrive);
     }
@@ -52,8 +45,10 @@ public class AutonomousCommand extends CommandBase {
 
     @Override
     public void execute(){
-        //aim
-        if(!shot && !aim && !turned && !intakeOn && !moved){
+        
+        switch(step){
+            //aim
+            case 0:
             if (autoaim.hasTarget() == 1) {
                 // while (aim.getXOffset() > 3) {
                 //   double x = aim.getXOffset();
@@ -74,147 +69,149 @@ public class AutonomousCommand extends CommandBase {
                 //   // wheel.setWheel(RPM);
                 // }
               }else{
-                  aim = true;
+                  step = 1;
               }
-
-        }
-
-        //shooting
-        if(!shot && aim && !turned && !intakeOn && !moved){
-        shooter.setWheel(power);
-        if (power >= 0.6) {
-            innerIndex.spin();
-            outerIndex.spin();
-            if(timer.get() >= 2){
-                shot = true;
-                shooter.setWheel(0);
-                innerIndex.stop();
-                outerIndex.stop();
-            }
-        }else{
-            power += 0.009;
-            timer.reset();
-             }
-        }
-
-        //turn around 180 degrees
-
-        if(!turned && shot && aim && !intakeOn && !moved){
-            if(swerveDrive.getAngle() < 180.0){
-                swerveDrive.updatePeriodic(0, 0, 0.3);
-            }else{
-                turned = true;
-            }
-            //yaw is in radians per second
-
-        }
-
-        //turn on intake
-
-        if(turned && shot && aim && !intakeOn && !moved){
-            intake.extend();
-            intake.spinIntake();
-            outerIndex.spin();
-            intakeOn = true;
-
-        }
-
-        //move forward several meters
-
-        if(turned && shot && aim && intakeOn && !moved){
-            if((swerveDrive.getDriveDistance()/2.58) * 0.023876 > 4){
-                swerveDrive.updatePeriodic(0, 0, 0);
-                moved = true;
-            }else{
-                swerveDrive.updatePeriodic(0, 0.3, 0);
-            }
-        }
-
-        //retract intake, turn off outer index
-
-        if(turned && shot && aim && intakeOn && moved && !retractedIntake){
-            intake.stopIntake();
-            intake.retract();
-            outerIndex.stop();
-            retractedIntake = true;
-        }
-
-        
-
-        //turn around again 
-
-        if(turned && shot && aim && intakeOn && moved && retractedIntake && !turnedAgain){
-            if(swerveDrive.getAngle() < 360.0){
-                swerveDrive.updatePeriodic(0, 0, 0.3);
-            }else{
-                turnedAgain = true;
-            }
-        }
-
-        //autoaim
-
-        if(shot && turned && aim && intakeOn && moved && retractedIntake && turnedAgain && !aim2){
-            if (autoaim.hasTarget() == 1) {
-                // while (aim.getXOffset() > 3) {
-                //   double x = aim.getXOffset();
-                //   if (x < 0) {
-                //     swerveDriveTrain.updatePeriodic(0, 0, 0.35);
-                //   } else {
-                //     swerveDriveTrain.updatePeriodic(0, 0, -0.35);
-                //   }
-                // }
-        
-                double angle = autoaim.setHoodAim();
-                // double angle = 15;
-                hood.setHoodAngle(angle);
-                SmartDashboard.putNumber("angle", angle);
-                System.out.println("Setting to: " + angle);
-                // if (Constants.controller.getBButton()) {
-                //   double RPM = aim.getRPM();
-                //   // wheel.setWheel(RPM);
-                // }
-              }else{
-                  aim2 = true;
-              }
-
-        }
-
-        //shoot again 
-
-        if(shot && turned && aim && intakeOn && moved && retractedIntake && turnedAgain && aim2 && !shoot2){
-            shooter.setWheel(power);
-            if (power >= 0.6) {
+              
+            break;
+              //shoot
+            case 1:
+              shooter.setWheel(power);
+                if (power >= 0.6) {
                 innerIndex.spin();
                 outerIndex.spin();
-                if(timer.get() >= 2){
-                    shot = true;
+                    if(timer.get() >= 2){
+                    step = 2;
                     shooter.setWheel(0);
                     innerIndex.stop();
                     outerIndex.stop();
-                }
-            }else{
+                    }
+                }else{
                 power += 0.009;
                 timer.reset();
-                 }
-            }
+                }
 
+            break;
 
+                //turn around 180 degrees
+            case 2:
+                if(swerveDrive.getAngle() < 180.0){
+                    swerveDrive.updatePeriodic(0, 0, 0.3);
+                }else{
+                    step = 3;
+                }
+            break;
 
+            //turn on intake
+            case 3:
+                intake.extend();
+                intake.spinIntake();
+                outerIndex.spin();
+                step = 4;
+            break;
 
+            //move forward several meters
 
+            case 4:
+                if((swerveDrive.getDriveDistance()/2.58) * 0.023876 > 4){
+                    swerveDrive.updatePeriodic(0, 0, 0);
+                    step = 5;
+                }else{
+                    swerveDrive.updatePeriodic(0, 0.3, 0);
+                }
+
+            break;
+
+            
+            //retract intake, turn off outer index
+
+            case 5:
+                intake.stopIntake();
+                intake.retract();
+                outerIndex.stop();
+                step = 6;
+            break;
+
+            //turn around again
+            case 6:
+                if(swerveDrive.getAngle() < 360.0){
+                    swerveDrive.updatePeriodic(0, 0, 0.3);
+                }else{
+                    swerveDrive.updatePeriodic(0,0,0);
+                    step = 7;
+                }
+            break;
+
+            //autoaim again 
+
+            case 7:
+                if (autoaim.hasTarget() == 1) {
+                    // while (aim.getXOffset() > 3) {
+                    //   double x = aim.getXOffset();
+                    //   if (x < 0) {
+                    //     swerveDriveTrain.updatePeriodic(0, 0, 0.35);
+                    //   } else {
+                    //     swerveDriveTrain.updatePeriodic(0, 0, -0.35);
+                    //   }
+                    // }
         
-          
+                    double angle = autoaim.setHoodAim();
+                    // double angle = 15;
+                    hood.setHoodAngle(angle);
+                    SmartDashboard.putNumber("angle", angle);
+                    System.out.println("Setting to: " + angle);
+                    // if (Constants.controller.getBButton()) {
+                    //   double RPM = aim.getRPM();
+                    //   // wheel.setWheel(RPM);
+                    // }
+                }else{
+                  step = 8;
+                }
+
+            break;
+
+
+            //shoot again
+
+            case 8:
+
+                shooter.setWheel(power);
+                if (power >= 0.6) {
+                    innerIndex.spin();
+                    outerIndex.spin();
+                    if(timer.get() >= 2){
+                        step = 9;
+                        shooter.setWheel(0);
+                        innerIndex.stop();
+                        outerIndex.stop();
+                    }
+                }else{
+                    power += 0.009;
+                    timer.reset();
+                }
+
+            break;
+
+            default:
+            step = 9;
+            break;
+
+
+
+        }
+        
+
 
     }
 
     @Override
     public void end(boolean interrupted){
+
         
     }
 
     @Override
     public boolean isFinished(){
-        return false;
+        return (step >= 9);
     }
 
     
